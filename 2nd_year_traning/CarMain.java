7f6e97ea7d10d7633e14c8703efa8350d2d67e3a
sjyuvaraj <yuvarajsj2004@gmail.com>
2025-09-24 16:02:47 +0530

    24/09/25

---
+++ 2nd_year_traning/CarMain.java
@@ -0,0 +1,19 @@
+
+public class CarMain{
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.name = "Tata Punch";
+        car1.fuel = "Petrol";
+        // car1.price = 549999.00;
+        // car1.brand= "Tata";
+        car1.setBrand("Tata");
+        car1.setPrice(549999.00);
+        System.out.println("Car Details are as follows:");
+        System.out.println("Name of the Car: " + car1.name);
+        System.out.println("Brand: " + car1.getBrand());
+        System.out.println("Fuel Type: " + car1.fuel);
+        System.out.println("Offer Price: " + car1.getPrice());
+    }
+}
+
+
